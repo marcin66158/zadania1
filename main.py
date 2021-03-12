f31227d828b2e71a640c5d1870e0a08667c5069b
Marcin66158 <marcin-rybacki12@wp.pl>
2021-03-12 14:42:50 +0100

    zad1

--- main.py
+++ main.py
@@ -1,16 +1,72 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+print("Zadanie 1")
+zmienna1=1
+zmienna2=2
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+print(zmienna1,zmienna2)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Zadanie 2 ")
+
+
+print("Zadanie 3")
+print("Podaj e")
+e = 7
+wynik = e**10
+
+print("%.2f" %(wynik))
+print("Zadanie 4")
+imie='MARCIN'
+nazwisko='RYBACKI'
+
+print(imie.capitalize())
+print(nazwisko.capitalize())
+
+print("Zadanie 5")
+
+piosenka = "lalalaj lalalaj lalalala lalala lajlajlaj"
+
+iloscLaj = piosenka.count("laj")
+print(iloscLaj)
+
+print("Zadanie 6")
+imie = "Marcin"
+
+print("Druga litera %s, ostatnia litera %s" %(imie[1], imie[len(imie)-1]))
+
+print("Zadanie 7")
+imie = "Marcin"
+
+print(imie.split("r"))
+
+print("Zadanie 8")
+
+zmienna1 = "Marcin"
+zmienna2 = 1.5
+zmienna3 = 0xF
+
+print("zmienna1 = %s zmienna2 = %f zmienna3 = %i" %(zmienna1, zmienna2, zmienna3))
+
+print("Zadanie 9")
+
+sport = ["siatkowka", "pilka nozna", "koszykowka"]
+print(sport)
+sport.reverse()
+print(sport)
+sport.append("bieganie")
+sport.append("tenis")
+print(sport)
+
+print("Zadanie 10")
+
+itp = "i tym podobne"
+itd = "i tak dalej"
+np = "na przyklad"
+wg = "wedlug"
+slownikSkrotow = {
+'itp': "i tym podobne",
+'itd': 'i tak dalej',
+'np': 'na przyklad',
+'wg': 'wedlug'}
+
+print(slownikSkrotow['wg'])
+
